4e9af1495f971b6405860a4ed008e3e8edc5a1d1
MatthewOglesby <matt.oglesby99@gmail.com>
2022-08-28 14:52:29 -0500

    first commit
    first commit

---
+++ build/precache-manifest.bcde0bd3e3d040978a114530753b624f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12bc33727ff8ca6f71a15f3a44f1497",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4c860bee24d247e0be4",
+    "url": "/static/js/2.b560fc1f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b560fc1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f58a196559524676a21b",
+    "url": "/static/js/main.092dec65.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.092dec65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eec87231792ee23208a",
+    "url": "/static/js/runtime-main.ee1d87d1.js"
+  }
+]);
